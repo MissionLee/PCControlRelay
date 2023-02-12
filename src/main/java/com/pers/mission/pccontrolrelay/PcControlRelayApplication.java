1d28df87d51641dbd2dbc9a11f79cde2c56b1038
zuixu <zuixue3000@163.com>
2023-02-12 16:17:01 +0800

    origin commit

---
+++ src/main/java/com/pers/mission/pccontrolrelay/PcControlRelayApplication.java
@@ -0,0 +1,13 @@
+package com.pers.mission.pccontrolrelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcControlRelayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcControlRelayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pers/mission/pccontrolrelay/PcControlRelayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pers.mission.pccontrolrelay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcControlRelayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
